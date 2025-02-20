75bb3a572f37890d7e2a062326b0fda3ea4aa9cb
ADMIN <ADMIN@DESKTOP-M57JVOT>
2025-02-21 01:20:23 +0530

    ReactiveForms TemplateDriven empty files

---
+++ src/app/components/template-driven/template-driven.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-template-driven',
+  standalone: true,
+  imports: [],
+  templateUrl: './template-driven.component.html',
+  styleUrl: './template-driven.component.scss'
+})
+export class TemplateDrivenComponent {
+
+}
